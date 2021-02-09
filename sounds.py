6fc5d4a1ad1242ea540167ac30ad55a82e04c230
Willy Kim <wkim149@gmail.com>
2021-02-09 15:54:38 -0500

    skip days w/o new episodes

--- sounds.py
+++ sounds.py
@@ -2,6 +2,7 @@ import requests
 import xml.etree.ElementTree as ET
 import vlc
 import time
+from datetime import datetime
 
 def get_first_item(url):
 	xml = requests.get(url).content
@@ -14,11 +15,31 @@ def wait_for_player(player):
 	while str(player.get_state()) in good_states:
 		time.sleep(5)
 
+def get_day():
+	return datetime.today().strftime('%a')
+
+def get_upfirst():
+	# no upfirst on sundays
+	if get_day() == 'Sun':
+		return None
+
+	return get_first_item('https://feeds.npr.org/510318/podcast.xml')
+
+def get_shortwave():
+	# no shortwave on sats, suns
+	if get_day() in ['Sat', 'Sun']:
+		return None
+
+	return get_first_item('https://feeds.npr.org/510351/podcast.xml')
+
 def main():
-	upfirst = get_first_item('https://feeds.npr.org/510318/podcast.xml')
-	shortwave = get_first_item('https://feeds.npr.org/510351/podcast.xml')
+	upfirst = get_upfirst()
+	shortwave = get_shortwave()
 
 	for audio in [upfirst, shortwave]:
+		if audio is None:
+			continue
+
 		player = vlc.MediaPlayer(audio)
 		player.play()
 
