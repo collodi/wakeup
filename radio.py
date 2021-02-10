bcc74c6d448e00f1ad754be1ebb6ac4e5140db87
Willy Kim <wkim149@gmail.com>
2021-02-10 11:06:58 -0500

    start at radio volume 0

--- radio.py
+++ radio.py
@@ -6,9 +6,9 @@ def main():
 	player = vlc.MediaPlayer(url)
 	player.play()
 
-	for i in range(70):
-		player.audio_set_volume(30 + i)
-		time.sleep(3)
+	for i in range(100):
+		player.audio_set_volume(i)
+		time.sleep(2.4)
 
 	player.audio_set_volume(100)
 	time.sleep(60)
